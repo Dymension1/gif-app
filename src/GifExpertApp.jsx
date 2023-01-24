5791cba7da72764805a4e4ff870191fde4612533
Luis <Luisurias27@gmail.com>
2023-01-23 18:26:22 -0700

    gifs

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState([""]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    //categories.push(newCategory);
+    setCategories([newCategory, ...categories]);
+    //setCategories( cat => [ ...cat, 'lol' ] )
+  };
+
+  return (
+    <>
+      <div className="main">
+        <h1>
+          <a href={""}>GIPHY SEARCH</a>
+        </h1>
+
+        <AddCategory onNewCategory={(value) => onAddCategory(value)} />
+
+        <p>
+          by <strong>Luis</strong>
+          <br />
+          follow me in my social media <br />
+          <a href="https://twitter.com/Luisur27">
+            <img className="twitter" src="/images/Twitter-logo.svg.png"></img>
+          </a>
+          <a href="https://www.instagram.com/luisur66/">
+            <img className="insta" src="/images/logo-insta.png"></img>
+          </a>
+        </p>
+
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </div>
+    </>
+  );
+};
